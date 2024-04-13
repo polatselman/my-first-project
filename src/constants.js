300ad94a71044fefa764642e35045cb9c8352277
polatselman <selman.polat60@gmail.com>
2024-04-14 00:43:56 +0200

    minigame of my portfolio
    Explore my portfolio and discover a captivating 2D mini-game! Dive into a world of colorful characters and engaging gameplay. This mini-game showcases my creativity and passion for game development. Play, enjoy, and let the fun begin!

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my computer. Lately, I've been primarily working with JavaScript and TypeScript.
+    I've made one game right now and hope to do more. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/polatselman" target="_blank">Github</a>!`,
+  "cs-degree": `This is an empty picture frame. I hung it on the wall because I'm aiming to put my CS degree in it.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. At times, I also complete some of my tasks here.`,
+  tv: `That's my TV. I've been watching tech and fitness youtubers a lot recently like :
+   <a href="https://www.youtube.com/@Smoljames" target="_blank">Smoljames</a>, <a href="https://www.youtube.com/@AsmrProg" target="_blank">AsmrProg</a>,
+  <a href="https://www.youtube.com/@ardasaatci1357" target="Arda Saatçi</a> (a lot) and <a href="https://www.youtube.com/@yvguo" target="_blank">yvguo</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `On my desk, you'll find CAD plans—a reflection of my journey as a civil engineer. They symbolize my expertise and dedication, each line a testament to my vision for the future.`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `In my home library, German and Turkish books line the shelves, reflecting my multilingual journey (yes I speak three languages). As I immerse myself in their pages, I deepen my connection to diverse cultures. Now, Arabic texts await, marking my ongoing pursuit of linguistic and cultural enrichment.`,
+  exit: `If you want to exit polatselman's portfolio, just close the tab.`,
+};
